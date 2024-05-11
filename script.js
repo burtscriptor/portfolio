1a7b6356dce61266523c26088cd10c8e5d8a12fb
David Burt <burt.david.j@gmail.com>
2024-05-11 16:21:14 +1000

    ongoing edits

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  // todo
+  // switch around about me section image and text
+  // rewrite experience section
+  // lose the borders on the tooolbox divs?
+  // set up msg for contact me
+
+  // projects
+  // things up links
+  // work out how to put text over images
